1299734d94cff3266492f8166112493426979247
Einon Cris O.Alcantara <aeinoncris@gmail.com>
2025-09-17 22:07:42 +0800

    File Tracker

---
+++ FrontEnd/File-Tracker/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function App() {
+  return (
+    <>
+      <h1 className="text-3xl">Welcome to File Tracker</h1>
+      <p className="text-lg">This is a file tracking application.</p>
+    </>
+  )
+}
+
+export default App
+
